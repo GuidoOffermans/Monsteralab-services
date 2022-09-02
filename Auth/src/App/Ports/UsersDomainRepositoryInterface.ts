5b7e23754a39aca7bce64a1d583a001ab1fc003e
guido offermans <guidooffermans@hotmail.com>
2022-09-02 16:38:57 +0200

    add findById repository method

--- Auth/src/App/Ports/UsersDomainRepositoryInterface.ts
+++ Auth/src/App/Ports/UsersDomainRepositoryInterface.ts
@@ -1,8 +1,11 @@
 import { EmailAddress } from "../../Domain/Values/EmailAddress";
 import { User } from "../../Domain/User";
+import { UUID } from "../../Domain/Values/UUID";
 
 export interface UsersDomainRepositoryInterface {
 	findOne(email: EmailAddress): Promise<User>;
+
+	findById(id: UUID): Promise<User>;
 
 	save(user: User): void;
 }
--- Auth/src/Infrastructure/UsersDomainRepository.ts
+++ Auth/src/Infrastructure/UsersDomainRepository.ts
@@ -4,14 +4,31 @@ import { DataSource } from "typeorm";
 import { UUID } from "../Domain/Values/UUID";
 import { User } from "../Domain/User";
 import { Password } from "../Domain/Values/Password";
-import { User as OrmUser } from "../Framework/Models/User";
 import { EmailAddress } from "../Domain/Values/EmailAddress";
 import { UserNotFoundError } from "../Domain/Errors/UserNotFoundError";
+import { User as OrmUser } from "../Framework/Models/User";
 import { UsersDomainRepositoryInterface } from "../App/Ports/UsersDomainRepositoryInterface";
 
 @Injectable()
 export class UsersDomainRepository implements UsersDomainRepositoryInterface {
-	public constructor(private dataSource: DataSource) {}
+	public constructor(private dataSource: DataSource) {
+	}
+
+	public async findById(id: UUID): Promise<User> {
+		const ormUser = await (await this.getUsersSelectQueryBuilder())
+			.where("user.id = :id", { id: id.value })
+			.getOne();
+
+		if (!ormUser) {
+			throw new UserNotFoundError();
+		}
+
+		return new User(
+			new UUID(ormUser.id),
+			new EmailAddress(ormUser.email),
+			new Password(ormUser.password),
+		);
+	}
 
 	public async findOne(email: EmailAddress): Promise<User> {
 		const ormUser = await (await this.getUsersSelectQueryBuilder())
